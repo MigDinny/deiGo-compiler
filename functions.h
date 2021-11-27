b488ac050ca24a5485b0f9093b458b49135d7d04
Alexy de Almeida <65732412+Squalexy@users.noreply.github.com>
2021-11-27 13:04:31 +0000

    3 functions done (#7)
    * 3 functions done
    
    1 - symtab_look
    2 - symtab_find_duplicate
    3 - throwErrorDeclaredButNeverUsed
    
    TODO:
    - lines and columns on 3rd function
    
    * ligeiro ajuste

--- functions.h
+++ functions.h
@@ -20,6 +20,6 @@ void printNotedTree(node_t *root, int init_depth);
 void printTables(symtab_t *global);
 void printTableElements(elem_t * element);
 
-elem_t* symtab_look(symtab_t *tab, symtab_t *global, char *id);
+elem_t* symtab_look(char *tabname, symtab_t *global, char *id);
 int symtab_find_duplicate(symtab_t *tab, char *id);
 void throwErrorDeclaredButNeverUsed(symtab_t *global);
